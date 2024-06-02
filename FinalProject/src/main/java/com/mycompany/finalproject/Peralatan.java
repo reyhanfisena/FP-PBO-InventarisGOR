45dbd93418499da5cd9b21413c277eb965053b32
reyhanfisena <115385745+reyhanfisena@users.noreply.github.com>
2024-06-02 10:14:10 +0700

    new file

---
+++ FinalProject/src/main/java/com/mycompany/finalproject/Peralatan.java
@@ -0,0 +1,44 @@
+package com.mycompany.finalproject;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author REYHANFISENA
+ */
+public class Peralatan {
+    protected String nama;
+    protected String id;
+    protected int jumlah;
+
+    public Peralatan(String nama, String id, int jumlah) {
+        this.nama = nama;
+        this.id = id;
+        this.jumlah = jumlah;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+
+    @Override
+    public String toString() {
+        return nama + " (ID: " + id + ", Jumlah: " + jumlah + ")";
+    }
+}
+
