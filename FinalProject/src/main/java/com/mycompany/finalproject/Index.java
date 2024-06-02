e7a3431fd865140d0dbe5403738f56cb068f8f27
reyhanfisena <115385745+reyhanfisena@users.noreply.github.com>
2024-06-02 09:54:26 +0700

    add file

---
+++ FinalProject/src/main/java/com/mycompany/finalproject/Index.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.finalproject;
+
+/**
+ *
+ * @author REYHANFISENA
+ */
+public class Index {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
